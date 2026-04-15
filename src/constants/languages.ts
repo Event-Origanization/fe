eabca54b692a59df620945275f39bd28181ce01d
tuandevdt <doanthanhtuan1998@gmail.com>
2026-04-15 20:29:38 +0700

    refactor: update UI select language & change slogan

--- src/constants/languages.ts
+++ src/constants/languages.ts
@@ -1,7 +1,7 @@
 export const LANGUAGES = [
   { code: 'vi', name: 'VN', flag: 'https://flagcdn.com/w20/vn.png' },
-  { code: 'en', name: 'EN', flag: 'https://flagcdn.com/w20/us.png' },
-  { code: 'zh', name: 'ZH', flag: 'https://flagcdn.com/w20/cn.png' },
+  { code: 'en', name: 'EN', flag: 'https://flagcdn.com/w20/gb.png' },
+  { code: 'zh', name: 'CN', flag: 'https://flagcdn.com/w20/cn.png' },
 ] as const
 
 export type LanguageCode = typeof LANGUAGES[number]['code']
